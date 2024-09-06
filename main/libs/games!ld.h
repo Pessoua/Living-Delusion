05be0eeffae0b891433901708246208bd1572086
Pessoua <tomasmr2007@gmail.com>
2024-09-06 18:23:20 +0100

    testing if the new pc works :>

--- main/libs/games!ld.h
+++ main/libs/games!ld.h
@@ -1,11 +1,13 @@
 #ifndef LD_GAMES_H
 #define LD_GAMES_H
 
-/*int Animal(bool is_tut);
-void Crop(const short int Crop1_ID, bool is_tut);
-void Fish(const short int Fish1_ID, const char *what_pond, bool is_tut);
-void Tree(bool is_invasive, const char *num_spot, const short int Log1_ID, const short int special_tree_ID, bool is_tut);
-void Key(const char *Caller);
-void SpecialLocations(const char * Caller);*/
+/*
+ * int Animal(bool is_tut);
+ * void Crop(const short int Crop1_ID, bool is_tut);
+ * void Fish(const short int Fish1_ID, const char *what_pond, bool is_tut);
+ * void Tree(bool is_invasive, const char *num_spot, const short int Log1_ID, const short int special_tree_ID, bool is_tut);
+ * void Key(const char *Caller);
+ * void SpecialLocations(const char * Caller);
+ */
 
 #endif
