8bd9e8df13e27f19e4dd68f40be6e68c516e662c
Pessoua <tomasmr2007@gmail.com>
2024-09-12 01:58:43 +0100

    notes for later on extras

--- main/libs/extras!ld.c
+++ main/libs/extras!ld.c
@@ -1,5 +1,11 @@
 #include "../LivingDelusion.h"
 
+/* ´
+ * Instead of extras having the like scanf thing we make it so its a function that ONLY takes input like:
+ * Extras(const char * command);
+ * And also make it so extras IS the troubleshooting manual too (from ../../tools)
+ */
+
 void test_extras(){
     printf("extras is working\n");
     return;
