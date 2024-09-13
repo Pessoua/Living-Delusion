3d94a8206a6b67ccc495da6f3c0f5710c043dcd6
Pessoua <tomasmr2007@gmail.com>
2024-09-13 15:15:02 +0100

    Changed some things in the main LD lib

--- main/LivingDelusion.h
+++ main/LivingDelusion.h
@@ -14,9 +14,23 @@
 #include <stdbool.h>
 #include <sys/stat.h> 
 #include <errno.h>
+#include <stdint.h>
 
 //extern vars
 
+//typedefs go here (YES I STOLE THESE TYPEDEFS!)
+typedef int8_t      i8;         //From (-128 to 127)
+typedef uint8_t     u8;         //From (0 - 255)
+typedef int16_t     i16;        //From (-32 768 to 32 767)
+typedef uint16_t    u16;        //From (0 to 65 535)
+typedef int32_t     i32;        //From (-2 147 483 648 to 2 147 483 647)
+typedef uint32_t    u32;        //From (0 to 4 294 967 295)
+typedef int64_t     i64;        //From (-9 223 372 036 854 775 808 to 9 223 372 036 854 775 807)
+typedef uint64_t    u64;        //From (0 to 18 446 744 073 709 551 615)
+                                
+typedef float       f6;         //Example: 3.141593
+typedef double      f18;        //Example: 3.141592653589793116
+//^Examples for float and double stolen from here - (https://stackoverflow.com/questions/5098558/float-vs-double-precision)
 
 //extern libs here
 #include "libs/style!ld.h"       //Text formating + Making things look better
@@ -39,7 +53,7 @@
 #ifdef _WIN32
     #define CLR system("cls");
 #else
-    #define CLR system("clear");
+    #define CLR system("clear")https://stackoverflow.com/questions/5098558/float-vs-double-precision;
 #endif
 
 #endif
